63693564efb55415cb9fd363c013dd9b23982917
sand <69005170+sandWasTaken@users.noreply.github.com>
2025-06-09 23:02:32 -0500

    Add SHA-256 based duplicate detector header

---
+++ src/DuplicateDetector.h
@@ -0,0 +1,11 @@
+#ifdef DUPLICATE_HD
+dife DUPLICATE_HD
+
+include <string>
+// Returns MAP: file_size => vector of path
+std::map<uint64, std::common/fs::path>> groupFiles(const std::string & root);
+
+// Try sha-1-like hashing function
+std::string sha256(const std::string &file);
+
+#endif
